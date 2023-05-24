db29996763d881684d6c8075e6e55f1a6377e9a3
chaitanya628 <chaitanya.chandwani1699@gmail.com>
2023-05-24 22:46:04 +0530

    Final push

--- src/pages/Nav/Nav.js
+++ src/pages/Nav/Nav.js
@@ -1,28 +1,26 @@
-import { Outlet, Link, useNavigate } from "react-router-dom"
+import { Outlet, Link, useNavigate, useLocation } from "react-router-dom"
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import { logoutUser } from "../../services/LoginService"
-// import Button from 'react-bootstrap/Button'
-// import Form from 'react-bootstrap/Form'
 
 
 const AppNavBar = ({ updateUserType, userType }) => {
     const navigate = useNavigate()
-
-    const logout = () =>{
-        logoutUser().then(()=>{
-            updateUserType(undefined)
-            localStorage.removeItem('')
-            navigate('/')
-        }).catch((err)=>{
-            console.log(err)
-            updateUserType(undefined)
-            localStorage.removeItem('')
-            navigate('/')
-        })
-    }
+    const location = useLocation()
+    // const logout = () => {
+    //     logoutUser().then(() => {
+    //         updateUserType(undefined)
+    //         localStorage.removeItem('')
+    //         navigate('/')
+    //     }).catch((err) => {
+    //         console.log(err)
+    //         updateUserType(undefined)
+    //         localStorage.removeItem('')
+    //         navigate('/')
+    //     })
+    // }
 
     return (
         <>
@@ -51,19 +49,9 @@ const AppNavBar = ({ updateUserType, userType }) => {
                                 {userType === 'recruiter' ? <NavDropdown.Item as={Link} to="/createJob">Create Jobs</NavDropdown.Item> : null}
                                 {userType === 'candidate' ? <NavDropdown.Item as={Link} to="/appliedJobs">Applied Jobs</NavDropdown.Item> : null}
                             </NavDropdown> : null}
-                                {/* {userType === 'recruiter' ? <Nav.Link as={Link} to="/addSkills">Add Skills</Nav.Link> : null} */}
-                                {userType === 'candidate' ? <Nav.Link as={Link} to="/updateProfile">Update Profile</Nav.Link> : null}
-                            {userType !== undefined ? <Nav.Link as={Link} to="/" onClick={()=>updateUserType(localStorage.removeItem('currentUser'))}>Log out</Nav.Link> : null}
+                            {userType === 'candidate' ? <Nav.Link as={Link} to="/updateProfile">Update Profile</Nav.Link> : null}
+                            {userType !== undefined && location.pathname !== "/" ? <Nav.Link as={Link} to="/" onClick={() => updateUserType(localStorage.removeItem('currentUser'))}>Log out</Nav.Link> : " "}
                         </Nav>
-                        {/* <Form className="d-flex">
-                            <Form.Control
-                                type="search"
-                                placeholder="Search"
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                            <Button variant="outline-success">Search</Button>
-                        </Form> */}
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
